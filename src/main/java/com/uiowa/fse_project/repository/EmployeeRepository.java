8af14d4ac0f994fd136aa831825f0e1da8df0ba4
tjsubaric <91640793+tsubaric@users.noreply.github.com>
2023-04-08 23:31:13 -0500

    little warnings

--- src/main/java/com/uiowa/fse_project/repository/EmployeeRepository.java
+++ src/main/java/com/uiowa/fse_project/repository/EmployeeRepository.java
@@ -3,10 +3,8 @@ package com.uiowa.fse_project.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import com.uiowa.fse_project.model.Employee;
 
-@Repository
 public interface EmployeeRepository extends JpaRepository <Employee, Long>{
     Optional<Employee> findById(long id);
 }

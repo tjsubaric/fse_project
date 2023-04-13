128e8ccc952cd133eef8c8669837b5ab1d8af3f8
tjsubaric <91640793+tsubaric@users.noreply.github.com>
2023-04-13 13:38:33 -0500

    crud boards working

--- src/main/java/com/uiowa/fse_project/model/UserDtls.java
+++ src/main/java/com/uiowa/fse_project/model/UserDtls.java
@@ -13,9 +13,7 @@ public class UserDtls {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
-
-	//private String username;
+	private long id;
 
 	private String firstName;
 

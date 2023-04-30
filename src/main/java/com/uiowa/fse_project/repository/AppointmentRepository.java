f6766d6787062e084721ed14272a11147e8e310d
James Ostrowski <jamesostrowski21@gmail.com>
2023-04-30 17:35:31 -0500

    Query, missing save

--- src/main/java/com/uiowa/fse_project/repository/AppointmentRepository.java
+++ src/main/java/com/uiowa/fse_project/repository/AppointmentRepository.java
@@ -1,7 +1,14 @@
 package com.uiowa.fse_project.repository;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.uiowa.fse_project.model.Appointments;
 
-public interface AppointmentRepository extends JpaRepository<Appointments, Long>{
 
+public interface AppointmentRepository extends JpaRepository<Appointments, Long>{
+    @Query("SELECT a FROM Appointments a WHERE a.doctor = :doctorName")
+    List<Appointments> findByDoctorName(@Param("doctorName") String doctorName);
 }

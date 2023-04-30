353783c5a44dbffed3991484a5616db8e7ad44ea
Sam Maschmann <smaschmann@uiowa.edu>
2023-04-30 03:37:04 -0500

    added medInfo for the patient to see when diag/prs

--- src/main/java/com/uiowa/fse_project/controller/PatientController.java
+++ src/main/java/com/uiowa/fse_project/controller/PatientController.java
@@ -8,9 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import com.uiowa.fse_project.model.Patient;
 import com.uiowa.fse_project.model.UserDtls;
 import com.uiowa.fse_project.repository.UserRepository;
+import com.uiowa.fse_project.repository.PatientRepository;
 
 @Controller
 @RequestMapping("/patient")
@@ -19,12 +20,24 @@ public class PatientController {
 	@Autowired
 	private UserRepository userRepo;
 
+	@Autowired
+	private PatientRepository patientRepo;
+
 	@ModelAttribute
 	private void userDetails(Model m, Principal p) {
 		String email = p.getName();
 		UserDtls user = userRepo.findByEmail(email);
 
 		m.addAttribute("patient", user);
+
+	}
+
+	@ModelAttribute
+	private void patientDetails(Model m, Principal p) {
+		String email = p.getName();
+		Patient patient = patientRepo.findByEmail(email);
+
+		m.addAttribute("patientuser", patient);
 
 	}
 
@@ -36,6 +49,16 @@ public class PatientController {
 	@GetMapping("/appointments")
 	public String appointments() {
 		return "patient/appointments";
+	}
+
+	@GetMapping("/medInfo")
+	public String medInfo() {
+		return "patient/medInfo";
+	}
+
+	@GetMapping("/viewBill")
+	public String viewBill() {
+		return "patient/viewBill";
 	}
 
 }
